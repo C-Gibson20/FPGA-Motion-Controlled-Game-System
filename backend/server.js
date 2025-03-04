985c1b21ab674504f4e56acdc6fa7c613fdaa344
Junto Mabuchi <juntomabuchi@hotmail.com>
2025-03-04 16:33:16 +0000

    Initial Test Backend

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+require("dotenv").config(); // environment variables
+const express = require("express"); // http requests
+const {Pool} = require("pg"); // Postgres
+const cors = require("cors"); // Allows frontend to communicate w/ backend
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+// Loads .env variables
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+app.get("/users", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM users");
+        res.json(result.rows);
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+const PORT = 5001;
+app.listen(PORT, () => {
+    console.log("Server running on port 5001")
+});
