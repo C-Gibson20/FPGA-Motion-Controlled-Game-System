eafc3180178ad26555a27d96b64fc53b9055d327
C-Gibson20 <117122578+C-Gibson20@users.noreply.github.com>
2025-03-15 19:50:44 +0000

    debugging

--- backend/server/__main__.py
+++ backend/server/__main__.py
@@ -6,8 +6,11 @@ from game_manager import GameManager
 from fpga_server import start_tcp_server
 from websocket_server import start_ws_server
 
+async def main():
+    game = GameManager()
+    await asyncio.gather(start_ws_server(game), start_tcp_server())
+
 if __name__ == "__main__":
     init_db()
     threading.Thread(target=lambda: app.run(host="0.0.0.0", port=5000), daemon=True).start()
-    game = GameManager()
-    asyncio.run(asyncio.gather(start_ws_server(game), start_tcp_server()))
+    asyncio.run(main())
